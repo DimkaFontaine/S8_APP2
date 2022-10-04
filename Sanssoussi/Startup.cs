df7ea8945c0947577486db9968eab0a93aebbec3
DimkaFontaine <dimfontainee@gmail.com>
2022-10-04 18:31:28 -0400

    fix openID

--- Sanssoussi/Startup.cs
+++ Sanssoussi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,11 @@ namespace Sanssoussi
                 googleOptions.ClientId = this.Configuration["Authentication:Google:ClientId"];
                 googleOptions.ClientSecret = this.Configuration["Authentication:Google:ClientSecret"];
             });
+
+            services.Configure<CookiePolicyOptions>(options => {
+            	options.CheckConsentNeeded = context => true;
+            	options.MinimumSameSitePolicy = SameSiteMode.Lax;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +49,7 @@ namespace Sanssoussi
 
             app.UseStaticFiles();
 
+            app.UseCookiePolicy();
             app.UseRouting();
             app.UseAuthentication();
 
